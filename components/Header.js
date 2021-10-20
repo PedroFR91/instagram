ac507f05fee9ce1bf266b67b9dddfbf384502659
DevPed <pedromfr91@gmail.com>
2021-10-20 05:19:51 +0200

    Right section Header

--- components/Header.js
+++ components/Header.js
@@ -8,10 +8,11 @@ import {
   PaperAirplaneIcon,
   MenuIcon,
 } from '@heroicons/react/outline';
+import { HomeIcon } from '@heroicons/react/solid';
 function Header() {
   return (
     <div>
-      <div className='flex justify-between max-w-6xl'>
+      <div className='flex justify-between max-w-6xl mx-5 lg:mx-auto'>
         {/*Left*/}
         <div className='relative hidden lg:inline-grid w-24 cursor-pointer'>
           <Image
@@ -27,20 +28,44 @@ function Header() {
             objectFit='contain'
           />
         </div>
-
         {/*Middle*/}
-        <div className='relative mt-1 p-3 rounded-md'>
-          <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
-            <SearchIcon className='h-5 w-5 text-gray-500' />
-          </div>
-          <input
-            className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black
+        <div className='max-w-xs'>
+          <div className='relative mt-1 p-3 rounded-md'>
+            <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+              <SearchIcon className='h-5 w-5 text-gray-500' />
+            </div>
+            <input
+              className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black
             rounded-md'
-            type='text'
-            placeholder='Buscar'
-          />
+              type='text'
+              placeholder='Buscar'
+            />
+          </div>
         </div>
         {/*Right*/}
+        <div className='flex items-center justify-end space-x-4'>
+          <HomeIcon className='navBtn' />
+          <MenuIcon className='h-6 md:hidden cursor-pointer' />
+          <div className='relative navBtn'>
+            <PaperAirplaneIcon className='navBtn rotate-45' />
+            <div
+              className='absolute -top-1 -right-2 text-xs w-5 h-5 
+            bg-red-500 rounded-full flex items-center justify-center 
+            animate-pulse text-white'
+            >
+              3
+            </div>
+          </div>
+
+          <PlusCircleIcon className='navBtn' />
+          <UserGroupIcon className='navBtn' />
+          <HeartIcon className='navBtn' />
+          <img
+            src='https://www.pinclipart.com/picdir/big/165-1653686_female-user-icon-png-download-user-colorful-icon.png'
+            alt='profile pic'
+            className='h-10 rounded-full cursor-pointer'
+          />
+        </div>
       </div>
     </div>
   );
