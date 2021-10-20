79b54d2c303c455068e8e176a5ee121a216d1d01
DevPed <pedromfr91@gmail.com>
2021-10-20 05:38:36 +0200

    Feed component

---
+++ components/Feed.js
@@ -0,0 +1,19 @@
+import Stories from './Stories';
+
+function Feed() {
+  return (
+    <main classname='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto'>
+      <section className='col-span-2'>
+        {/*Stories*/}
+        <Stories />
+        {/*Posts*/}
+      </section>
+      <section>
+        {/*Mini Profile*/}
+        {/*Suggestions*/}
+      </section>
+    </main>
+  );
+}
+
+export default Feed;
