35f1d34e73db2447e1e87bf9c23ff869243c877d
DevPed <pedromfr91@gmail.com>
2021-12-01 01:50:23 +0100

    Signin Page 2

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,15 +1,30 @@
 import { getProviders, signIn as SignIntoProvider } from 'next-auth/react';
+import Header from '../../components/Header';
 
 function signIn({ providers }) {
   return (
     <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button onClick={() => SignIntoProvider(provider.id)}>
-            Sign in with {provider.name}
-          </button>
+      <Header />
+      <div className='flex flex-col items-center justify-center min-h-screen py-2 -mt-40 px-14 text-center'>
+        <img className='w-80' src='https://links.papareact.com/ocw' alt='' />
+        <p className='font-xs italic'>
+          Esta no es una aplicacion real, su unico propósito es formativo.
+        </p>
+        <div className='mt-40'>
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className='p-3 bg-blue-500 rounded-lg text-white'
+                onClick={() =>
+                  SignIntoProvider(provider.id, { callbackUrl: '/' })
+                }
+              >
+                Iniciar sesión con {provider.name}
+              </button>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </>
   );
 }
