e1281da21d133939346235ddb25e04d81f5c0f8d
DevPed <pedromfr91@gmail.com>
2021-12-03 13:09:28 +0100

    Post Component

--- components/Posts.js
+++ components/Posts.js
@@ -1,38 +1,34 @@
 import Post from './Post';
-const posts = [
-  {
-    id: '123',
-    username: 'Pedro',
-    userImg:
-      'https://www.pinclipart.com/picdir/big/165-1653686_female-user-icon-png-download-user-colorful-icon.png',
-    img: 'https://viajes.nationalgeographic.com.es/medio/2021/03/12/entre-las-mejores-del-mundo_b9cadbb5_752x501.jpg',
-    caption: 'Hello! How are you?',
-  },
-  {
-    id: '123',
-    username: 'Pedro',
-    userImg:
-      'https://www.pinclipart.com/picdir/big/165-1653686_female-user-icon-png-download-user-colorful-icon.png',
-    img: 'https://viajes.nationalgeographic.com.es/medio/2021/03/12/entre-las-mejores-del-mundo_b9cadbb5_752x501.jpg',
-    caption: 'Hello! How are you?',
-  },
-];
+import React, { useState, useEffect } from 'react';
+import { db } from '../firebase';
+import { collection, onSnapshot, orderBy, query } from '@firebase/firestore';
 
 function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    [db]
+  );
+
   return (
     <div>
-      <div>
-        {posts.map((post) => (
-          <Post
-            key={post.id}
-            id={post.id}
-            username={post.username}
-            userImg={post.userImg}
-            img={post.img}
-            caption={post.caption}
-          />
-        ))}
-      </div>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
+        />
+      ))}
     </div>
   );
 }
